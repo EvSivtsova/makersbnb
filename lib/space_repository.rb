f375eef2d436c282ac620a39f56edffe241d4732
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-19 13:05:30 +0100

    Refactor space_repository in line with rubocop

--- lib/space_repository.rb
+++ lib/space_repository.rb
@@ -4,27 +4,34 @@ class SpaceRepository
   def all
     sql = "SELECT * FROM spaces;"
     result_set = DatabaseConnection.exec_params(sql, [])
-    return nil if result_set.to_a.length == 0
-    map_PG_to_objects(result_set) # returns array of space objects
+    return nil if result_set.to_a.empty?
+    map_pg_to_objects(result_set) # returns array of space objects
   end
 
   def find_by_host_id(host_id)
     sql = "SELECT * FROM spaces WHERE host_id = $1;"
     result_set = DatabaseConnection.exec_params(sql, [host_id])
-    return nil if result_set.to_a.length == 0
-    map_PG_to_objects(result_set) # returns array of space objects
+    return nil if result_set.to_a.empty?
+    map_pg_to_objects(result_set) # returns array of space objects
   end
 
   def find_by_title(title)
     sql = "SELECT * FROM spaces WHERE title = $1;"
     result_set = DatabaseConnection.exec_params(sql, [title])
-    return nil if result_set.to_a.length == 0
-    map_PG_to_objects(result_set) # returns array of space objects
+    return nil if result_set.to_a.empty?
+    map_pg_to_objects(result_set) # returns array of space objects
   end
 
   def create(space)
-    sql = "INSERT INTO spaces (title, description, address, price_per_night, available_from, available_to, host_id) VALUES ($1, $2, $3, $4, $5, $6, $7);"
-    DatabaseConnection.exec_params(sql, [space.title, space.description, space.address, space.price_per_night, space.available_from, space.available_to, space.host_id])
+    sql = "INSERT 
+              INTO spaces (title, description, address, price_per_night, 
+                  available_from, available_to, host_id) 
+              VALUES ($1, $2, $3, $4, $5, $6, $7);"
+    DatabaseConnection.exec_params(sql, [
+      space.title, space.description, 
+      space.address, space.price_per_night, 
+      space.available_from, space.available_to, 
+      space.host_id])
     return nil
   end
 
@@ -55,23 +62,23 @@ class SpaceRepository
   def find_by_space_id(space_id)
     sql = "SELECT * FROM spaces WHERE space_id = $1;"
     result_set = DatabaseConnection.exec_params(sql, [space_id])
-    return nil if result_set.to_a.length == 0
+    return nil if result_set.to_a.empty?
     result = result_set[0]
-    record_to_object(result)
+    assign_space(result)
   end
 
   private
 
-  def map_PG_to_objects(result_set)
+  def map_pg_to_objects(result_set)
     spaces = []
     result_set.each do |record|
-      space = record_to_object(record)
+      space = assign_space(record)
       spaces << space
     end
     spaces
   end
 
-  def record_to_object(record)
+  def assign_space(record)
     space = Space.new
     space.space_id = record["space_id"]
     space.title = record["title"]
