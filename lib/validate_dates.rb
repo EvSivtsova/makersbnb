d63e6af06c043b417a67b18d1da64b0ae7cb28ac
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-21 15:49:06 +0100

    Add 5 test - validate_dates - Date.parse

--- lib/validate_dates.rb
+++ lib/validate_dates.rb
@@ -1,5 +1,11 @@
 def check_dates_within_availability_range?(start_date, end_date, available_from, available_to)
-  if (start_date < end_date) && (start_date > available_from) && (end_date < available_to)
+  start_date_object = Date.parse(start_date) 
+  end_date_object = Date.parse(end_date) 
+  available_from_object = Date.parse(available_from)
+  available_to_object = Date.parse(available_to)
+  if (start_date_object < end_date_object) && 
+      (start_date_object > available_from_object) && 
+      (end_date_object < available_to_object)
     return true
   else
     return false
--- spec/validate_dates_spec.rb
+++ spec/validate_dates_spec.rb
@@ -47,4 +47,15 @@ describe "check the dates are within the availability range" do
       expect(result).to be(false)
     end
   end
+
+  context "when comparing the dates in different formats" do
+    it "returns true when the start and end dates are within the range" do
+      start_date = "01-03-2022"
+      available_from = "2022/02/01"
+      end_date = "01-04-2022"
+      available_to = "2022/05/01"
+      result = check_dates_within_availability_range?(start_date, end_date, available_from, available_to)
+      expect(result).to be(true)
+    end
+  end
 end
