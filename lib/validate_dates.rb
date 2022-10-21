8dfc07f715abf872c08ea0b1131b6efd2cac11fb
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-21 15:05:47 +0100

    Add test validate_dates - hardcoded

---
+++ lib/validate_dates.rb
@@ -0,0 +1,3 @@
+def check_dates_within_availability_range?(start_date, end_date, available_from, available_to)
+  return true
+end
---
+++ spec/validate_dates_spec.rb
@@ -0,0 +1,14 @@
+require "validate_dates"
+
+describe "check the dates are within the availability range" do
+  context "when comparing date strings in the same format" do
+    it "returns true when the start and end dates are within the range" do
+      start_date = "2022/03/01"
+      available_from = "2022/02/01"
+      end_date = "2022/04/01"
+      available_to = "2022/05/01"
+      result = check_dates_within_availability_range?(start_date, end_date, available_from, available_to)
+      expect(result).to be(true)
+    end
+  end
+end
